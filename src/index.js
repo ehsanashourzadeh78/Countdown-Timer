95322332607c8e95a780c1d985d1419ccaa226a1
Ehsan <ehsan.ashourzadeh0078@gmail.com>
2024-08-05 11:25:01 +0330

    final commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./style.css"
+
+function App() {
+  return (
+    <div className='main'>
+      <Hello />
+      <Timer />
+    </div>
+  );
+}
+
+function Timer() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(intervalId); // Clean up interval on unmount
+  }, []);
+
+  return <h2 className='timer'>It is {time}</h2>;
+}
+
+function Hello() {
+  return <h1>Welcome My Friend</h1>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +35,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
