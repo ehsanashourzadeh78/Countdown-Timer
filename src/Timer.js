6ef116ab601b31bdd3d38da3e01b257a4f39729f
Ehsan <ehsan.ashourzadeh0078@gmail.com>
2024-08-07 08:34:13 +0330

    second commit

--- src/Timer.js
+++ src/Timer.js
@@ -1,33 +1,27 @@
 
-import React from "react";
-import "./style.css";
-import { render } from "@testing-library/react";
-class Timer extends React.Component{
-  constructor(){
-    super();
-    this.state ={
-  
-      time : 300
-    }
-  }
-  componentDidMount(){
+import React, { useEffect, useState } from "react";
+import "./style.css"
+
+export default function App() {
+  const [time, setTime] = useState(300);
+
+  useEffect(() => {
     let timer = setInterval(() => {
-      this.setState((time) => {
+      setTime((time) => {
         if (time === 0) {
           clearInterval(timer);
           return 0;
         } else return time - 1;
       });
-    }, 1000)
-  }
-  render(){
-    return(
-      <h2 className="timer">
-        it is {this.state.time}
-      </h2>
-    )
-  }
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="App">
+      <p>
+        Time left: {`${Math.floor(time / 60)}`.padStart(2, 0)}:
+        {`${time % 60}`.padStart(2, 0)}
+      </p>
+    </div>
+  );
 }
-
-
-  export default Timer;
